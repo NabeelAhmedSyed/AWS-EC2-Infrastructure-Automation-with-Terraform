7ec883ffded51b8e3e160bb4b3d094ff0d7a39de
Nabeel Ahmed Syed <syednabeel52119@gmail.com>
2024-10-27 07:17:24 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,17 @@ variable "ami" {
   description = "Amazon Machine Image ID"
   default     = "ami-0c55b159cbfafe1f0"
 }
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Amazon Machine Image ID"
+  default     = "ami-0c55b159cbfafe1f0"
+}
